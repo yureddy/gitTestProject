04ffa2f63b2fabb8323dbeeb53bdc2e78da68c95
Yugandar <ygndr.reddy@gmail.com>
2020-04-01 17:29:44 -0400

    Adding the programmig practise file of JS,Py,Swift and Cpp for the first time

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	printf(" I am a disco dancer");
+}
+
---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+print('Hello World!')
+
+x =1.2
+y =3.4
+
+print(x)
+
+print("\n India \n ")
+
+print("\n")
+print(x+y)
